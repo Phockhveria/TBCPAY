de84018564058f9c49db2d5dc0d28b75b7d6ad4c
Phockhveria <g.phockhveria@gmail.com>
2015-12-12 06:25:28 -0500

    Update TbcPayTerminal.php

--- Source/TbcPayTerminal.php
+++ Source/TbcPayTerminal.php
@@ -3,12 +3,19 @@
 class TbcPayTerminal {
 
 	private $User;
+
 	private $Amount;
+	
 	private $OrderID;
+
 	public $FailUser  = "მომხმარებელი არ არსებობს";
+	
 	public $OkUser    = "მომხმარებელი არსებობს";
+	
 	public $OkPay     = "გადახდა წარმატებით განხორციელდა";
+
 	public $FailPay   = "გადახდა ვერ განხორციელდა";
+
 	public $OrderFail = "ტრანზაკცია უკვე არსებობს";
 
 
@@ -17,85 +24,84 @@ class TbcPayTerminal {
 	function __construct()
 	{
 
-	   $this->User     =  null;
-	   $this->Amount   =  null;
-	   $this->OrderID  =  null;
+  	     $this->User     =  null;
+	     $this->Amount   =  null;
+	     $this->OrderID  =  null;
 
-	}
+	 }
 
 
-	function SecurityCheck()
+	public function SecurityCheck()
 	{
-    	$hostip = $_SERVER['REMOTE_ADDR'];
+        
+            $hostip = $_SERVER['REMOTE_ADDR'];
 
-	  if( $hostip === "92.241.77.174" || 
-	      $hostip === "95.104.112.74"  ) 
-      {
+	    if( $hostip === "92.241.77.174" || 
+	        $hostip === "95.104.112.74"  ) 
+            {
 
-	   return 1;
+	       	    return 1;
 
-	  }	
+	     }	
 
-	   exit(); 
+	     exit(); 
 
 	}
 	
 
-	function InitializePayment()
+       public function InitializePayment()
 	{
 
-	  if( isset($_GET["account"])    && 
-	      isset($_GET["amount"])     && 
-	      isset($_GET["orderid"])    ) 
-    	{	  
+	       if( isset($_GET["account"])    && 
+	           isset($_GET["amount"])     && 
+	           isset($_GET["orderid"])    ) 
+    	       {	  
 
-      	 $this->User     =  $_GET["user"];
-      	 $this->Amount   =  $_GET["amount"];
-     	 $this->OrderID  =  $_GET["orderid"];
+      	      		$this->User     =  $_GET["user"];
+      	          	$this->Amount   =  $_GET["amount"];
+     	        	$this->OrderID  =  $_GET["orderid"];
+                 	return 1; 
 
-	     return 1; 
+	        }	 
 
-	    }	
-
-	  exit();
+	        exit();
 
 	}
 
 
 
 
-	function InitilizeCheckUser()
+        public	function InitilizeCheckUser()
 	{
 
-	  if( isset($_GET["user"]) ) 
-	  {	  
+	         if( isset($_GET["user"]) ) 
+	         {	  
 
-	    $this->User     =  $_GET["user"];
-	    $this->Amount   =  null;
-	    $this->OrderID  =  null;
+	             $this->User     =  $_GET["user"];
+	             $this->Amount   =  null;
+	             $this->OrderID  =  null;
+	             return 1;  
 
-	    return 1;  
+	          }	
 
-	  }	
-
-	   exit();
+	          exit();
 
 	}
 
 
 
-	function ShowOkUser($login)
+        public	function ShowOkUser($login)
 	{
 
-	   $message = '<?xml version="1.0" encoding="utf-8"?><response>';
-	   $message .= '<result>105</result>';
-	   $message .= '<info>';
-	   $message .= '<extra name="username">'.$login.'</extra>';
-	   $message .= '</info>';
-	   $message .= '<comment>'.$this->OkUser.'</comment>';
-	   $message .= '</response>';
+	   	$message = '<?xml version="1.0" encoding="utf-8"?><response>';
+	   	$message .= '<result>105</result>';
+	   	$message .= '<info>';
+	   	$message .= '<extra name="username">'.$login.'</extra>';
+	   	$message .= '</info>';
+	   	$message .= '<comment>'.$this->OkUser.'</comment>';
+	   	$message .= '</response>';
 	   
-	   echo $message;
+	   	echo $message;
 
 	}
 
@@ -104,11 +110,11 @@ class TbcPayTerminal {
 	function ShowFailUser()
 	{
 
-  	   $message  = '<?xml version="1.0" encoding="utf-8"?> <response>';
-	   $message .= '<result>101</result>';
-	   $message .= '<comment>'.$this->OkUser.'</comment>';
-	   $message .= '</response>';
-	   echo $message;	
+  		 $message  = '<?xml version="1.0" encoding="utf-8"?> <response>';
+	   	 $message .= '<result>101</result>';
+		 $message .= '<comment>'.$this->OkUser.'</comment>';
+	   	 $message .= '</response>';
+	         echo $message;	
 
 	}
 
@@ -118,11 +124,11 @@ class TbcPayTerminal {
 	function ShowOkPay()
 	{
 
-	   $message = '<?xml version="1.0" encoding="utf-8"?> <response>';
-	   $message .= '<result>205</result>';
-	   $message .= '<comment>'.$this->OkUser.'</comment>';
-	   $message .= '</response>';
-	   echo $message;	
+	   	$message = '<?xml version="1.0" encoding="utf-8"?> <response>';
+		$message .= '<result>205</result>';
+	   	$message .= '<comment>'.$this->OkUser.'</comment>';
+	   	$message .= '</response>';
+	   	echo $message;	
 
 	}
 
@@ -131,11 +137,11 @@ class TbcPayTerminal {
 	function ShowOrderFail()
 	{
 
-	   $message = '<?xml version="1.0" encoding="utf-8"?> <response>';
-	   $message .= '<result>201</result>';
-	   $message .= '<comment>'.$this->OrderFail.'</comment>';
-	   $message .= '</response>';
-	   echo $message;
+	   	$message = '<?xml version="1.0" encoding="utf-8"?> <response>';
+	   	$message .= '<result>201</result>';
+	   	$message .= '<comment>'.$this->OrderFail.'</comment>';
+	   	$message .= '</response>';
+	  	echo $message;
 
 	}
 
@@ -143,11 +149,11 @@ class TbcPayTerminal {
 	function ShowError()
 	{
 
-	   $message  = '<?xml version="1.0" encoding="utf-8"?> <response>';
-	   $message .= '<result>300</result>';
-	   $message .= '<comment>მოხდა შეცდომა</comment>';
-	   $message .= '</response>';
-    	echo $message;	
+	   	$message  = '<?xml version="1.0" encoding="utf-8"?> <response>';
+	   	$message .= '<result>300</result>';
+	   	$message .= '<comment>მოხდა შეცდომა</comment>';
+	   	$message .= '</response>';
+    		echo $message;	
 
 	}
 
